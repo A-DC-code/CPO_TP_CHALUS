17a17543dbcd03f687ff8217cf102e16ea9bb27e
Antoine de_chalus <antoine.dechalus@epfedu.fr>
2025-10-22 12:12:54 +0200

    Création d'un nouveau projet TP0 helloword

---
+++ TP0_ helloWorld/src/tp0_/helloworld/TP0_HelloWorld.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp0_.helloworld;
+
+import java.util.Scanner;
+
+/**20/10/2025
+ *le role du programme est l'affichege d'un message
+ * @author antoine de chalus
+ */
+public class TP0_HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println( " Bonjour " ) ; 
+        String prenom;
+    Scanner sc ;
+    sc = new Scanner (System.in) ;
+    System.out.println("Quel est votre prénom ?");
+    prenom = sc.nextLine();
+        System.out.println( " Au revoir " ) ;
+        int i = 20;
+    }
+    
+}
