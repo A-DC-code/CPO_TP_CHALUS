995b2987ee72a3ce2889fb41cc081f36142c44bf
Antoine de_chalus <antoine.dechalus@epfedu.fr>
2025-10-24 12:22:31 +0200

    Création d'un nouveau projet TP1 convertisseur de température

--- TP1_convertisseur_DECHALUS/src/tp1_convertisseur_dechalus/TP1_convertisseur_DECHALUS.java
+++ TP1_convertisseur_DECHALUS/src/tp1_convertisseur_dechalus/TP1_convertisseur_DECHALUS.java
@@ -4,8 +4,10 @@
  */
 package tp1_convertisseur_dechalus;
 
-/**
- *
+import java.util.Scanner;
+
+/**24/10/2025
+ *ce programme est un convertisseur de température
  * @author chalu
  */
 public class TP1_convertisseur_DECHALUS {
@@ -14,7 +16,77 @@ public class TP1_convertisseur_DECHALUS {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Bonjour, saisissez une valeur :");
+        double valeur = sc.nextDouble();
+
+        System.out.println("Saisissez la conversion que vous souhaitez effectuer :");
+        System.out.println("1) De Celcius vers Kelvin");
+        System.out.println("2) De Kelvin vers Celcius");
+        System.out.println("3) De Farenheit vers Celcius");
+        System.out.println("4) De Celcius vers Farenheit");
+        System.out.println("5) De Kelvin vers Farenheit");
+        System.out.println("6) De Farenheit vers Kelvin");
+
+        int choix = sc.nextInt();
+
+        double resultat = 0;
+
+        switch (choix) {
+            case 1:
+                resultat = CelciusVersKelvin(valeur);
+                System.out.println(valeur + " degres Celcius est egal à " + resultat + " degres Kelvin");
+                break;
+            case 2:
+                resultat = KelvinVersCelcius(valeur);
+                System.out.println(valeur + " degres Kelvin est egal à " + resultat + " degres Celcius");
+                break;
+            case 3:
+                resultat = FarenheitVersCelcius(valeur);
+                System.out.println(valeur + " degres Farenheit est egal à " + resultat + " degres Celcius");
+                break;
+            case 4:
+                resultat = CelciusVersFarenheit(valeur);
+                System.out.println(valeur + " degres Celcius est egal à " + resultat + " degres Farenheit");
+                break;
+            case 5:
+                resultat = KelvinVersFarenheit(valeur);
+                System.out.println(valeur + " degres Kelvin est egal à " + resultat + " degres Farenheit");
+                break;
+            case 6:
+                resultat = FarenheitVersKelvin(valeur);
+                System.out.println(valeur + " degres Farenheit est egal à " + resultat + " degres Kelvin");
+                break;
+            default:
+                System.out.println("Erreur : choix invalide.");
+        }
+
+        sc.close();
     }
-    
+
+    public static double CelciusVersKelvin(double tCelcius) {
+        return tCelcius + 273.15;
+    }
+
+    public static double KelvinVersCelcius(double tKelvin) {
+        return tKelvin - 273.15;
+    }
+
+    public static double FarenheitVersCelcius(double tFarenheit) {
+        return (tFarenheit - 32) / 1.8;
+    }
+
+    public static double CelciusVersFarenheit(double tCelcius) {
+        return tCelcius * 1.8 + 32;
+    }
+
+    public static double KelvinVersFarenheit(double tKelvin) {
+        return CelciusVersFarenheit(KelvinVersCelcius(tKelvin));
+    }
+
+    public static double FarenheitVersKelvin(double tFarenheit) {
+        return CelciusVersKelvin(FarenheitVersCelcius(tFarenheit));
+    }
 }
