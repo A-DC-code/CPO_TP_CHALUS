1d4d6ffe4e9b2451372afd7561e06b9cb78e2af1
Antoine de_chalus <antoine.dechalus@epfedu.fr>
2025-11-18 13:07:02 +0100

    Création d'un nouveau projet TP3 sur les magiciens

---
+++ TP3_Heroic_Fantasy_DECHALUS/src/Armes/Arme.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Armes;
+
+/**
+ *
+ * @author chalu
+ */
+public abstract class Arme {
+    String nom;
+    int attaque;
+    
+    public Arme(String nom,int attaque){
+        this.attaque=attaque;
+        this.nom=nom;
+    
+    public String getNom() {
+        return nom;
+    }
+}
